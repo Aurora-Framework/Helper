1d0fa09d641d994cf7d8136f4aff220342419804
VeeeneX <veeenex@gmail.com>
2016-01-25 15:31:01 +0100

    Support for php7

---
+++ src/Helper/Str.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Aurora\Helper;
+
+class Str
+{
+    const ALNUM   = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const ALPHA   = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const HEXDEC  = '0123456789abcdef';
+    const NUMERIC = '0123456789';
+
+    const SYMBOLS = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+    public static function limitWords($string, $limit = 15, $end = "")
+    {
+        return preg_replace('/((\w+\W*|| [\p{L}]+\W*){'.($limit-1).'}(\w+))(.*)/', '${1}', $string).$end;
+    }
+
+    public static function limitCharacters($string, $limit = 15, $end = "")
+    {
+        return preg_match('(\S{'.$limit.'})', $string).$end;
+    }
+
+    public static function censorWords($string, $disallowed = null, $replacement = "")
+    {
+        return str_replace($disallowed, $replacement, $string);
+    }
+
+    public static function slug($string, $keepCase = false, $replaceWith = "-")
+    {
+        if ($keepCase === false) $string = mb_strtolower($string);
+
+        $string = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+
+        $string = preg_replace("/[^a-z_\-A-Z0-9. ]/", "", $string);
+        $string = preg_replace("/[\s-]+/", " ", $string);
+        $string = preg_replace("/[\s_]/", $replaceWith, $string);
+
+        return $string;
+    }
+
+    public static function ascii($string)
+    {
+        return preg_replace('/[^\x0-\x7F]/', '', $string);
+    }
+
+    public static function mask($string, $visible = 3, $mask = '*')
+    {
+        if ($visible === 0) {
+            return str_repeat($mask, mb_strlen($string));
+        }
+
+        $visible = mb_substr($string, -$visible);
+
+        return str_pad($visible, (mb_strlen($string) + ( strlen($visible) - mb_strlen($visible) )), $mask, STR_PAD_LEFT);
+    }
+
+    public static function random($pool = String::ALNUM, $length = 32)
+    {
+        $string = '';
+        $poolSize = mb_strlen($pool) - 1;
+
+        for ($i = 0; $i < $length; $i++) {
+            $string .= mb_substr($pool, mt_rand(0, $poolSize), 1);
+        }
+
+        return $string;
+    }
+
+}
